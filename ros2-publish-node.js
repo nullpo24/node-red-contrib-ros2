80b8667af3f94442291c8c8bd877eda2f7e465bd
nullpo24 <hot.cake@live.jp>
2022-06-09 19:42:41 +0900

    subscriber working

--- ros2-publish-node.js
+++ ros2-publish-node.js
@@ -1,10 +1,26 @@
 module.exports = function(RED) {
     return function (config) {
+        const rclnodejs = require('rclnodejs');
+
         RED.nodes.createNode(this,config);
         var node = this;
-        this.on('input', function(msg) {
-            msg.payload = msg.payload.toRos2();
-            node.send(msg);
+
+        rclnodejs.init().then(() => {
+            const rclnode = rclnodejs.createNode('publisher_example_node');
+            const publisher = rclnode.createPublisher('std_msgs/msg/String', 'topic');
+
+            this.on('input', function(msg) {
+                node.warn(msg);
+                // publisher.publish(msg);
+                publisher.publish('hello world!');
+                // msg.payload = msg.payload.toRos2();
+            });
+
+            rclnodejs.spin(rclnode);
+        });
+
+        node.on("close", function() {
+          rclnodejs.shutdownAll();
         });
     }
 }
--- ros2-subscribe-node.js
+++ ros2-subscribe-node.js
@@ -1,34 +1,22 @@
 module.exports = function(RED) {
     return function (config) {
-      const rclnodejs = require('bindings')('rclnodejs');
+      const rclnodejs = require('rclnodejs');
   
       RED.nodes.createNode(this,config);
       var node = this;
-  
-      var subscriber;
-      var ros2;
 
-    //   node.server.on('ros2 connected', () => {
-    //       ros2 = rclnodejs.createNode('subscription_node');
+      rclnodejs.init().then(() => {
+        const rclnode = rclnodejs.createNode('subscription_example_node');
 
-    //       ros2.createSubscription('std_msgs/msg/String', 'topic', (msg) => {
-    //         console.log(`Received message: ${typeof msg}`, msg);
-    //       });
-
-    //       topicQuery(node.topic);
-
-    //   });
-
-        node.send('a-001');
-        rclnodejs.init().then(() => {
-          node.send('b-001');
-          const rclnode = rclnodejs.createNode('subscription_example_node');
-
-          rclnode.createSubscription('std_msgs/msg/String', 'chatter', (msg) => {
-            node.send(`Received message: ${typeof msg}`, msg);
+        rclnode.createSubscription('std_msgs/msg/String', 'chatter', (data) => {
+          node.send({payload: data});
         });
 
-        rclnodejs.spin(node);
+        rclnodejs.spin(rclnode);
+      });
+
+      node.on("close", function() {
+        rclnodejs.shutdownAll();
       });
     }
 }
